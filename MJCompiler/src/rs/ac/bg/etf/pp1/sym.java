40d25ff5f9a0c5b4c0ea806ee5c62b1f1a1ea1ee
Luka <sl180053d@student.etf.bg.ac.rs>
2022-01-03 01:33:37 +0100

    uradjena prva faza - Leksicka analiza, ujedno i prvi komit

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,65 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	// Keywords
+	public static final int PROG = 1;
+	public static final int PRINT = 2;
+	public static final int RETURN = 3;
+	public static final int BREAK = 4;
+	public static final int CLASS = 5;
+	public static final int ENUM = 6;
+	public static final int ELSE = 7;
+	public static final int CONST = 8;
+	public static final int IF = 9;
+	public static final int DO = 10;
+	public static final int WHILE = 11;
+	public static final int NEW = 12;
+	public static final int READ = 13;
+	public static final int EXTENDS = 14;
+	public static final int CONTINUE = 15;
+	public static final int THIS = 16;
+	public static final int SUPER = 17;
+	public static final int GOTO = 18;
+	public static final int RECORD = 19;
+
+	
+	// Identifiers
+	public static final int IDENT = 20;
+	
+	// Constants
+	public static final int NUMBER = 21;
+	public static final int CHARACTER = 22;
+	public static final int BOOLEAN = 23;
+	
+	// Operators
+	public static final int PLUS = 24;
+	public static final int MINUS = 25;
+	public static final int MULTIPLY = 26;
+	public static final int DEVIDE = 27;
+	public static final int EQUALEQUAL = 28;
+	public static final int NOTEQUAL = 29;
+	public static final int GREATEROREQUAL = 30;
+	public static final int LESSOREQUAL = 31;
+	public static final int AND = 32;
+	public static final int OR = 33;
+	public static final int EQUAL = 34;
+	public static final int INCREMENT = 35;
+	public static final int DECREMENT = 36;
+	public static final int SEMI = 37;
+	public static final int COLON = 38;
+	public static final int DOT = 39;
+	public static final int COMMA = 40;
+	public static final int LPAREN = 41;
+	public static final int RPAREN = 42;
+	public static final int LBRACKET = 43;
+	public static final int RBRACKET = 44;
+	public static final int LBRACE = 45;
+	public static final int RBRACE = 46;
+	public static final int LESS = 47;
+	public static final int GREATER = 48;
+	
+	public static final int EOF = 49;
+	public static final int VOID = 50;
+	
+}
