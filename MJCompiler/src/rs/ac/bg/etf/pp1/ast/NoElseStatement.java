dfa4e52dd75060a2231a38570ab54baaeafb4bdd
Luka <lukastojanovic314@gmail.com>
2022-01-16 20:16:28 +0100

    Poceo fazu generisanja koda

--- MJCompiler/src/rs/ac/bg/etf/pp1/ast/NoElseStatement.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/NoElseStatement.java
@@ -1,6 +1,6 @@
 // generated with ast extension for cup
 // version 0.8
-// 15/0/2022 22:30:19
+// 16/0/2022 20:13:9
 
 
 package rs.ac.bg.etf.pp1.ast;
--- MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.pp1;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -14,6 +15,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
 
 public class MJParserTest {
@@ -29,7 +31,8 @@ public class MJParserTest {
 
 		Reader br = null;
 		try {
-			File sourceCode = new File("test/test303.mj");
+			String fileName = "program";
+			File sourceCode = new File("test/mjCodes/" + fileName + ".mj");
 			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
 
 			br = new BufferedReader(new FileReader(sourceCode));
@@ -56,8 +59,16 @@ public class MJParserTest {
 			Tab.dump();
 
 			if (!p.errorDetected && v.passed()) {
+				File objFile = new File("test/objOutput/" + fileName + ".obj");
+				if (objFile.exists())
+					objFile.delete();
+				CodeGenerator codeGenerator = new CodeGenerator();
+				prog.traverseBottomUp(codeGenerator);
+				Code.dataSize = v.nVars;
+				Code.mainPc = codeGenerator.getMainPc();
+				Code.write(new FileOutputStream(objFile));
 				log.info("Parsiranje uspesno zavrseno!");
-			} else { 
+			} else {
 				log.error("Parsiranje NIJE uspesno zavrseno!");
 			}
 		} finally {
